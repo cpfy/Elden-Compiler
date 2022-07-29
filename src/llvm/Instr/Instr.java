a65716c0ba7bc674d10fc8dac8d55a77c0dd0e83
cpfy <309301477@qq.com>
2022-07-29 20:12:06 +0800

    style: 增加Instr全部元素类型注释

--- src/llvm/Instr/Instr.java
+++ src/llvm/Instr/Instr.java
@@ -9,6 +9,20 @@ public class Instr {
 
     private String instrname;   //N种中间代码种类
 
+    /* instr所有种类如下（仅目前，可能还会增加）：
+
+    [基本运算]: add fadd sub fsub mul fmul sdiv fdiv（均归属于BinaryInst类）
+    [运算扩展]: zext
+    [空间分配]: alloca
+    [元素赋值]: assign  (左为元素对象，右为另一个独立Instr，例如：%123 = load i32, i32* %3)
+    [存储加载]: load store
+    [函数调用]: call
+    [数组指针]: getelementptr
+    [比较跳转]: icmp
+    [跳转相关]: br condbr ret
+
+    */
+
     private String rawstr;  //输出的ircode字符串格式
     private String IRstring;
 
