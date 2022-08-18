e50a954291bffa1522ecf5cefbb09263eed5a88e
sjz_cirno <1601169949@qq.com>
2022-08-19 01:23:52 +0800

    [pass] 关闭除法优化（有bug），只开phi函数相关优化

--- src/pass/PassManager.java
+++ src/pass/PassManager.java
@@ -29,7 +29,7 @@ public class PassManager {
             new Rename(function);                 //变量重命名
             new SinglePhiDel(function);           //删除参数列表只有一个元素的phi函数，并将变量传播
 //            new ConstProp(function);              //常数折叠、局部公共子表达式删除、死代码删除
-//            new removePhi(function);
+            new removePhi(function);
 //            new UselessBlockDel(function);        //空基本块删除
 //            new removePhi(function);
 
